a013ee999c286ffc037be7d0be82e7c59acc7171
batu_takei <shunsukeika@gmail.com>
2025-10-30 00:07:31 +0900

    or

--- disjunction.lean
+++ disjunction.lean
@@ -1,22 +1,25 @@
-variable (p q r : Prop)
+variable (p q : Prop)
 
 #check Or.intro_left
 #check Or.intro_right
+
+-- p → p ∨ q
 example (hp : p) : p ∨ q := Or.intro_left q hp
+-- shorthand Or.intro_left:
+example (hp : p) : p ∨ q := Or.inl hp
+
+-- q → p ∨ q
 example (hq : q) : p ∨ q := Or.intro_right p hq
+-- shorthand Or.intro_right:
+example (hq : q) : p ∨ q := Or.inr hq
 
 #check Or.elim
+
+-- p ∨ q → q ∨ p
 example (h : p ∨ q) : q ∨ p :=
   Or.elim h
-    (fun hp : p => -- suppose p
-      show q ∨ p from Or.intro_right q hp) -- get q ∨ p from q by ∨ intro
-    (fun hq : q => -- suppose q
-      show q ∨ p from Or.intro_left p hq) -- get q ∨ p from q by ∨ intro
-
--- shorthand Or.intro:
-example (h : p ∨ q) : q ∨ p :=
-  Or.elim h (fun hp => Or.inr hp) (fun hq => Or.inl hq)
-
--- same mean:
+    (fun hp : p => show q ∨ p from Or.intro_right q hp)
+    (fun hq : q => show q ∨ p from Or.intro_left p hq)
+-- shorthand Or.elim:
 example (h : p ∨ q) : q ∨ p :=
   h.elim (fun hp => Or.inr hp) (fun hq => Or.inl hq)
