928eac2a46018721f4c12a09dfeaa5ce1adebcd0
batu_takei <shunsukeika@gmail.com>
2025-10-30 00:07:21 +0900

    and

--- conjunction.lean
+++ conjunction.lean
@@ -1,25 +1,32 @@
 variable (p q : Prop)
 
 #check And.intro
+
+-- p → q → p ∧ q
 example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
 #check λ (hp : p) (hq : q) => And.intro hp hq
+-- shorthand And.intro
+example (hp : p) (hq : q) : p ∧ q := ⟨ hp , hq ⟩
 
 #check And.left
 #check And.right
+
+-- p ∧ q → p
 example (h : p ∧ q) : p := And.left h
-example (h : p ∧ q) : q := And.right h
-
--- you can use below istead of And.intro
-example (hp : p) (hq : q) : p ∧ q := ⟨ hp , hq ⟩
-
--- you can use below istead of And.left and And.right
+-- shorthand And.left:
 example (h : p ∧ q) : p := h.left
+
+-- p ∧ q → p
+example (h : p ∧ q) : q := And.right h
+-- shorthand And.right:
 example (h : p ∧ q) : q := h.right
 
+-- p ∧ q → q ∧ p
 example (h : p ∧ q) : q ∧ p := And.intro (And.right h) (And.left h)
--- you can also be written:
+-- shorthand above:
 example (h : p ∧ q) : q ∧ p := ⟨ h.right, h.left ⟩
 
+-- p ∧ q → q ∧ p ∧ q
 example (h : p ∧ q) : q ∧ p ∧ q := ⟨ h.right, ⟨ h.left, h.right ⟩ ⟩
 -- you can flatten nestted constructors that associated to the right:
 example (h : p ∧ q) : q ∧ p ∧ q := ⟨ h.right, h.left, h.right ⟩
