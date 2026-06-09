79df0be2ffd277dda513fdab274881877d61dc20
batu_takei <shunsukeika@gmail.com>
2026-06-09 16:28:04 +0900

    tpl

--- math_project/test/tpl-5-10.lean
+++ math_project/test/tpl-5-10.lean
@@ -13,13 +13,37 @@ example : p ∨ False ↔ p := by
   · intro h
     apply Or.inl; assumption
 
+example : p ∧ False ↔ False := by
+  constructor
+  · intro ⟨_,_⟩; contradiction
+  · intro _; contradiction
 
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+example : (p → q) → (¬q → ¬p) := by
+  intro h₁ h₂ h₃
+  exact h₂ (h₁ h₃)
 
-example : (¬q → ¬p) → (p → q) := sorry
-example : p ∨ ¬p := sorry
-example : (((p → q) → p) → p) := sorry
+open Classical
+
+example : (¬q → ¬p) → (p → q) := by
+  intro h₁ h₂
+  have em := em q
+  cases em with
+  | inl h => assumption
+  | inr h =>
+    have := h₁ h h₂
+    contradiction
+
+example : p ∨ ¬p := by exact em p
+
+example : (((p → q) → p) → p) := by
+  intro h₁
+  cases Classical.em p with
+  | inl hp => exact hp
+  | inr hnp =>
+      apply h₁
+      intro hp
+      have := hnp hp
+      contradiction
 
 end chap3
 
@@ -28,7 +52,32 @@ section chap4
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
-example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
+  intro h x
+  cases h with
+  | inl g => apply Or.inl (g x)
+  | inr g => apply Or.inr (g x)
+
+open Classical
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  constructor
+  · intro h
+    have em := em r
+    cases em with
+    | inl hr => apply Or.inr hr
+    | inr hnr =>
+      apply Or.inl
+      intro x
+      cases (h x) with
+      | inl g => assumption
+      | inr g =>
+        have := hnr g
+        contradiction
+  · intro h x
+    cases h with
+      | inl g => apply Or.inl (g x)
+      | inr g => apply Or.inr g
 
 end chap4
 
